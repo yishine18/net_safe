d6cc7e8aee95971fa3454dbcd00b8ff5d12a2341
hero <hero@herodeMacBook-Air.local>
2020-02-21 10:12:32 +0800

    顺序交互，和服务器，接收到quit，就退出，没有用多线程。

--- mysocket/socket_client.py
+++ mysocket/socket_client.py
@@ -1,9 +1,45 @@
+# import socket
+
+# s2 = socket.socket()
+# s2.connect(("127.0.0.1",2345))
+
+# data = s2.recv(1024)
+# s2.close()
+# print ("receive data",repr(data))
+
 import socket
 
-s2 = socket.socket()
-s2.connect(("127.0.0.1",2345))
+#in python3.7
+#if in python2.X ,input() will can't work!
+def conn_socket():
+    client = socket.socket()
+    ip_port=('127.0.0.1',2466)
+    client.connect(ip_port)
+    data = client.recv(1024)
+    while True:
 
-data = s2.recv(1024)
-s2.close()
-print ("receive data",repr(data))
+        if data == b"quit":
+            print("client ready to quit!")
+            break
+        else:
+            print("this is from server:",data.decode())
+            msg_input = input("input :")
+            client.send(bytes(msg_input, encoding="gb2312"))
+            print("send data:",msg_input)
+            if msg_input == "quit":
+                print("client ready to quit!")
+                break
+        print("waiting to receive from server:")
+        data = client.recv(1024)
+    client.close()
 
+
+def test_001():
+    str1 = input("input str:")
+    str2 = str(str1)
+    print("str1 = ",str2)
+    print ("type str1 = ",type(str1))
+    print ("type str2 = ", type(str2))
+
+# test_001()
+conn_socket()
